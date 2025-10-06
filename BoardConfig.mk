e80f9f020cdf2982c9f5266c96dee25fc4705dc1
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2025-10-06 21:02:20 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,7 +137,7 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/librpmbclient                   \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/librpmbclient             \
  $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.0    \           
-    $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/android.hardware.keymaster@4.0 \
+ $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/android.hardware.keymaster@4.0 \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1      \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/android.hardware.keymaster@4.1  \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster-V2-ndk_platform \
