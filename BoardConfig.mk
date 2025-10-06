9bd73fd39dc3a71fca1d94b2cdf18bec62149a62
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2025-10-06 20:50:23 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,12 +132,16 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libkeymaster4_1support \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster_messages \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libkeymaster_messages \
-$(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster_portable \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster_portable \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libkeymaster_portable \
     $(TARGET_OUT_SHARED_LIBRARIES)/librpmbclient \
-    $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/librpmbclient \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.0 \
+    $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/librpmbclient
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.0 \           
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/android.hardware.keymaster@4.0 \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1 \
+    $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/android.hardware.keymaster@4.1  \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster-V2-ndk_platform \
+    $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/android.hardware.keymaster-V2-ndk_platform \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.gatekeeper@1.0 \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/android.hardware.gatekeeper@1.0
 
