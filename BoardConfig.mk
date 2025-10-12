fe5773a3be949c18012b2440e8a9e7faaecaadf4
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2025-10-12 22:14:59 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@
 DEVICE_PATH := device/realme/RMX3235
 
 # Architecture
-TARGET_BUILD_64BIT := true
+TARGET_BUILD_64BIT := false
 ifeq ($(TARGET_BUILD_64BIT), true)
 # Build 64-bit TWRP
 TARGET_ARCH := arm64
@@ -36,17 +36,16 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 else
 # Build 32-bit TWRP
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := cortex-a55
+TARGET_CPU_VARIANT_RUNTIME := generic
 endif
 
-TARGET_SUPPORTS_32_BIT_APPS := true
 # 64-bit
 TARGET_SUPPORTS_64_BIT_APPS := false
-TARGET_IS_64_BIT := false
+#TARGET_IS_64_BIT := false
 TARGET_USES_64_BIT_BINDER := true
 
 # Bootloader
