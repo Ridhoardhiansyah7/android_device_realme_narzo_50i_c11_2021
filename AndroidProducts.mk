1064703eed7f5ca0491008b8a913549a3c5ee921
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2025-11-07 10:55:12 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,8 +17,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_RMX3235.mk \
     $(LOCAL_DIR)/pbrp_RMX3235.mk \
-    $(LOCAL_DIR)/ofox_RMX3235.mk \
-    $(LOCAL_DIR)/shrp_RMX3235.mk
+    $(LOCAL_DIR)/ofox_RMX3235.mk
 
 COMMON_LUNCH_CHOICES := \
     twrp_RMX3235-user \
@@ -29,7 +28,4 @@ COMMON_LUNCH_CHOICES := \
     pbrp_RMX3235-eng \
     ofox_RMX3235-user \
     ofox_RMX3235-userdebug \
-    ofox_RMX3235-eng \
-    shrp_RMX3235-user \
-    shrp_RMX3235-userdebug \
-    shrp_RMX3235-eng
+    ofox_RMX3235-eng
